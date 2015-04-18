a53000826784a62057104cb0b0deef7253fdde6a
Yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-04-18 03:16:21 -0700

    Replaced zone entries with objects containing full name and the shorthand name for use as anchor links behind the scenes.

--- client/cartographer.js
+++ client/cartographer.js
@@ -4,75 +4,286 @@ if (Meteor.isClient) {
 
   Template.header.helpers({
     zones: [
-      { classic: "Ak'Anon", kunark: "Burning Woods", velious: "Cobalt Scar"},
-      { classic: "Befallen", kunark: "Cabilis (East)", velious: "Crystal Caverns"},
-      { classic: "Blackburrow", kunark: "Cabilis (West)", velious: "Dragon Necropolis"},
-      { classic: "Butcherblock Mountains", kunark: "Chardok", velious: "Eastern Wastes"},
-      { classic: "Cazic-Thule", kunark: "City of Mist", velious: "Iceclad Ocean"},
-      { classic: "Commonlands (East)", kunark: "Dalnir", velious: "Icewell Keep"},
-      { classic: "Commonlands (West)", kunark: "Dreadlands", velious: "Kael Drakkal"},
-      { classic: "Crushbone", kunark: "Emerald Jungle", velious: "Plane of Growth"},
-      { classic: "Dagnor's Cauldron", kunark: "Field of Bone", velious: "Plane of Mischief"},
-      { classic: "Erud's Crossing", kunark: "Firiona Vie", velious: "Siren's Grotto"},
-      { classic: "Erudin", kunark: "Frontier Mountains", velious: "Skyshrine"},
-      { classic: "Erudin (Palace)", kunark: "Howling Stones (Charasis)", velious: "Sleeper's Tomb"},
-      { classic: "Everfrost", kunark: "Kaesora", velious: "Temple of Veeshan"},
-      { classic: "Feerrott", kunark: "Karnor's Castle", velious: "The Great Divide"},
-      { classic: "Felwithe (North)", kunark: "Kurn's Tower", velious: "Thurgadin"},
-      { classic: "Felwithe (South)", kunark: "Lake of Ill Omen", velious: "Tower of Frozen Shadow"},
-      { classic: "Freeport (North)", kunark: "Overthere", velious: "Velketor's Labyrinth"},
-      { classic: "Freeport (East)", kunark: "Old Sebilis", velious: "Wakening Land"},
-      { classic: "Freeport (West)", kunark: "Mines of Nurga", velious: "Western Wastes"},
-      { classic: "Greater Faydark", kunark: "Skyfire Mountains", velious: ""},
-      { classic: "Grobb", kunark: "Swamp of No Hope", velious: ""},
-      { classic: "Guk (Bottom)", kunark: "Temple of Droga", velious: ""},
-      { classic: "Guk (Top)", kunark: "Timorous Deep", velious: ""},
-      { classic: "Halas", kunark: "Trakanon's Teeth", velious: ""},
-      { classic: "Highpass Hold", kunark: "Veeshan's Peak", velious: ""},
-      { classic: "Highpass Keep", kunark: "Warsliks Woods", velious: ""},
-      { classic: "Innothule Swamp", kunark: "", velious: ""},
-      { classic: "Kaladim (North)", kunark: "", velious: ""},
-      { classic: "Kaladim (South)", kunark: "", velious: ""},
-      { classic: "Karana (East)", kunark: "", velious: ""},
-      { classic: "Karana (North)", kunark: "", velious: ""},
-      { classic: "Karana (South)", kunark: "", velious: ""},
-      { classic: "Karana (West)", kunark: "", velious: ""},
-      { classic: "Kedge Keep", kunark: "", velious: ""},
-      { classic: "Kerra Island", kunark: "", velious: ""},
-      { classic: "Kithicor Woods", kunark: "", velious: ""},
-      { classic: "Lake Rathetear", kunark: "", velious: ""},
-      { classic: "Lavastorm Mountains", kunark: "", velious: ""},
-      { classic: "Lesser Faydark", kunark: "", velious: ""},
-      { classic: "Mistmoore Castle", kunark: "", velious: ""},
-      { classic: "Misty Thicket", kunark: "", velious: ""},
-      { classic: "Nagafen's Lair (Sol B)", kunark: "", velious: ""},
-      { classic: "Najena", kunark: "", velious: ""},
-      { classic: "Nektulos", kunark: "", velious: ""},
-      { classic: "Neriak (Commons)", kunark: "", velious: ""},
-      { classic: "Neriak (Foreign Quarter)", kunark: "", velious: ""},
-      { classic: "Neriak (Third Gate)", kunark: "", velious: ""},
-      { classic: "Northern Desert of Ro", kunark: "", velious: ""},
-      { classic: "Oasis of Marr", kunark: "", velious: ""},
-      { classic: "Oggok", kunark: "", velious: ""},
-      { classic: "Ocean of Tears", kunark: "", velious: ""},
-      { classic: "Paineel", kunark: "", velious: ""},
-      { classic: "Permafrost", kunark: "", velious: ""},
-      { classic: "Plane of Fear", kunark: "", velious: ""},
-      { classic: "Plane of Hate", kunark: "", velious: ""},
-      { classic: "Plane of Sky", kunark: "", velious: ""},
-      { classic: "Qeynos (North)", kunark: "", velious: ""},
-      { classic: "Qeynos (South)", kunark: "", velious: ""},
-      { classic: "Qeynos Aqueducts", kunark: "", velious: ""},
-      { classic: "Qeynos Hills", kunark: "", velious: ""},
-      { classic: "Rathe Mountains", kunark: "", velious: ""},
-      { classic: "Rivervale", kunark: "", velious: ""},
-      { classic: "Runnyeye Citadel", kunark: "", velious: ""},
-      { classic: "Solusek's Eye (Sol A)", kunark: "", velious: ""},
-      { classic: "Southern Desert of Ro", kunark: "", velious: ""},
-      { classic: "Steamfont Mountains", kunark: "", velious: ""},
-      { classic: "The Estate of Unrest", kunark: "", velious: ""},
-      { classic: "The Temple of Solusek Ro", kunark: "", velious: ""},
-      { classic: "Toxxulia Forest", kunark: "", velious: ""}]
+      { classic: {name: "Ak'Anon", who: "ohhh"}, 
+        kunark:  {name: "Burning Woods", who: ""},
+        velious: {name: "Cobalt Scar", who: ""}
+      },
+      { classic: {name: "Befallen", who: ""},
+				kunark:  {name: "Cabilis (East)", who: ""},
+				velious: {name: "Crystal Caverns", who: ""}
+			},
+      { classic: {name: "Blackburrow", who: ""},
+				kunark:  {name: "Cabilis (West)", who: ""},
+				velious: {name: "Dragon Necropolis", who: ""}
+			},
+      { classic: {name: "Butcherblock Mountains", who: ""},
+				kunark:  {name: "Chardok", who: ""},
+				velious: {name: "Eastern Wastes", who: ""}
+			},
+      { classic: {name: "Cazic-Thule", who: ""},
+				kunark:  {name: "City of Mist", who: ""},
+				velious: {name: "Iceclad Ocean", who: ""}
+			},
+      { classic: {name: "Commonlands (East)", who: ""},
+				kunark:  {name: "Dalnir", who: ""},
+				velious: {name: "Icewell Keep", who: ""}
+			},
+      { classic: {name: "Commonlands (West)", who: ""},
+				kunark:  {name: "Dreadlands", who: ""},
+				velious: {name: "Kael Drakkal", who: ""}
+			},
+      { classic: {name: "Crushbone", who: ""},
+				kunark:  {name: "Emerald Jungle", who: ""},
+				velious: {name: "Plane of Growth", who: ""}
+			},
+      { classic: {name: "Dagnor's Cauldron", who: ""},
+				kunark:  {name: "Field of Bone", who: ""},
+				velious: {name: "Plane of Mischief", who: ""}
+			},
+      { classic: {name: "Erud's Crossing", who: ""},
+				kunark:  {name: "Firiona Vie", who: ""},
+				velious: {name: "Siren's Grotto", who: ""}
+			},
+      { classic: {name: "Erudin", who: ""},
+				kunark:  {name: "Frontier Mountains", who: ""},
+				velious: {name: "Skyshrine", who: ""}
+			},
+      { classic: {name: "Erudin (Palace)", who: ""},
+				kunark:  {name: "Howling Stones (Charasis)", who: ""},
+				velious: {name: "Sleeper's Tomb", who: ""}
+			},
+      { classic: {name: "Everfrost", who: ""},
+				kunark:  {name: "Kaesora", who: ""},
+				velious: {name: "Temple of Veeshan", who: ""}
+			},
+      { classic: {name: "Feerrott", who: ""},
+				kunark:  {name: "Karnor's Castle", who: ""},
+				velious: {name: "The Great Divide", who: ""}
+			},
+      { classic: {name: "Felwithe (North)", who: ""},
+				kunark:  {name: "Kurn's Tower", who: ""},
+				velious: {name: "Thurgadin", who: ""}
+			},
+      { classic: {name: "Felwithe (South)", who: ""},
+				kunark:  {name: "Lake of Ill Omen", who: ""},
+				velious: {name: "Tower of Frozen Shadow", who: ""}
+			},
+      { classic: {name: "Freeport (North)", who: ""},
+				kunark:  {name: "Overthere", who: ""},
+				velious: {name: "Velketor's Labyrinth", who: ""}
+			},
+      { classic: {name: "Freeport (East)", who: ""},
+				kunark:  {name: "Old Sebilis", who: ""},
+				velious: {name: "Wakening Land", who: ""}
+			},
+      { classic: {name: "Freeport (West)", who: ""},
+				kunark:  {name: "Mines of Nurga", who: ""},
+				velious: {name: "Western Wastes", who: ""}
+			},
+      { classic: {name: "Greater Faydark", who: ""},
+				kunark:  {name: "Skyfire Mountains", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Grobb", who: ""},
+				kunark:  {name: "Swamp of No Hope", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Guk (Bottom)", who: ""},
+				kunark:  {name: "Temple of Droga", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Guk (Top)", who: ""},
+				kunark:  {name: "Timorous Deep", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Halas", who: ""},
+				kunark:  {name: "Trakanon's Teeth", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Highpass Hold", who: ""},
+				kunark:  {name: "Veeshan's Peak", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Highpass Keep", who: ""},
+				kunark:  {name: "Warsliks Woods", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Innothule Swamp", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Kaladim (North)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Kaladim (South)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Karana (East)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Karana (North)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Karana (South)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Karana (West)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Kedge Keep", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Kerra Island", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Kithicor Woods", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Lake Rathetear", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Lavastorm Mountains", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Lesser Faydark", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Mistmoore Castle", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Misty Thicket", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Nagafen's Lair (Sol B)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Najena", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Nektulos", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Neriak (Commons)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Neriak (Foreign Quarter)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Neriak (Third Gate)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Northern Desert of Ro", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Oasis of Marr", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Oggok", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Ocean of Tears", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Paineel", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Permafrost", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Plane of Fear", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Plane of Hate", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Plane of Sky", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Qeynos (North)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Qeynos (South)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Qeynos Aqueducts", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Qeynos Hills", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Rathe Mountains", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Rivervale", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Runnyeye Citadel", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Solusek's Eye (Sol A)", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Southern Desert of Ro", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Steamfont Mountains", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "The Estate of Unrest", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "The Hole", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "The Temple of Solusek Ro", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+			},
+      { classic: {name: "Toxxulia Forest", who: ""},
+				kunark:  {name: "", who: ""},
+				velious: {name: "", who: ""}
+      }]
   });
   Template.content.helpers({
     counter: function () {
