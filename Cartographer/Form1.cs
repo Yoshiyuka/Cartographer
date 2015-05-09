18c27c57bf30d2d00489b38ac6049a43738f0551
Yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-05-08 22:51:06 -0700

    Missing DLL file resolved by managing SQLite through nuget.

--- Cartographer/Form1.cs
+++ Cartographer/Form1.cs
@@ -16,24 +16,6 @@ namespace Cartographer
 
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
-        public static extern IntPtr GetDesktopWindow();
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr FindWindow(
-        [MarshalAs(UnmanagedType.LPTStr)] string lpClassName,
-        [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
-        [DllImport("user32.dll")]
-        public static extern IntPtr SetParent(
-             IntPtr hWndChild,      // handle to window
-             IntPtr hWndNewParent   // new parent window
-           );
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-        private const int GWL_EXSTYLE = -20;
-        private const int WS_EX_TRANSPARENT = 0x20;
-
         SQLiteConnection dbConnection;
 
 
