4293b5919cbb2fc29f2a0c4948eac2d7a2276b7f
Yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-05-31 23:10:09 -0700

    URL to be parsed wasn't quoted. Project should compile now.

--- scraper/src/scraper/core.clj
+++ scraper/src/scraper/core.clj
@@ -8,9 +8,10 @@
 (defn exp [y]
   (reduce * (repeat b y)))
 
+;we'll have to create a method to walk through the vector tree to extract tags/attributes
 (defn -main
   "Entry point to scraper methods. Pulls in data from project1999's wiki to insert into an SQLite DB."
   [& args]
   ;The main wiki page detailing the first 200 factions. This is where we will get a list of faction links to scrape
-  (tagsoup/parse http://wiki.project1999.com/index.php?title=Category:Factions)
+  (tagsoup/parse "http://wiki.project1999.com/index.php?title=Category:Factions")
       )
